b4a0476a8734119472422900bade599fea5c4909
Yogesh Rathi <yrathi89@gmail.com>
2021-10-15 15:42:02 +0530

    Added login page

---
+++ src/app/modules/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  
+  loginForm = new FormGroup({
+    clientId: new FormControl(undefined, [Validators.required]),
+    clientPassword: new FormControl(undefined, [Validators.required]),
+  });
+
+  generatePasswordForm = new FormGroup({
+    clientId: new FormControl(undefined, [Validators.required]),
+  });
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  generatePassword = () => {}
+  
+  forgetPassword = () => {}
+
+  login = (e: Event) => {
+    e.preventDefault();
+  }
+}
