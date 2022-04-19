bb9ea07a096137b548689ce28e14690e153e763e
Sunny Kumar <sunny@needleinnovision.com>
2022-04-19 21:31:40 +0530

    basic login test case and taking user to the admin customer side and trying to loop through the modal

---
+++ cypress/integration/cutomer-edit.spec.ts
@@ -0,0 +1,33 @@
+describe("login", () => {
+    it("Visits login page and enter details", () => {
+        cy.login('LC01', 'Yogesh@123');
+        // cy.url().should('includes', 'admin/portal');
+        // cy.get("h2").should("contain.text", "Home");
+        // cy.get('app-portal .cust').click();
+        // cy.url().should('includes', 'admin/customers');
+        // cy.get(".editcust").first().click();
+        // cy.get('.editcust').click({ multiple: true, force:true });
+        // cy.contains('a')
+        //     .parent()
+        //     .find('.editcust')
+        //     .click()
+
+
+
+
+    })
+
+    it("visits admin portal page", () => {
+        cy.url().should('includes', 'admin/portal');
+        cy.get("h2").should("contain.text", "Home");
+    })
+
+    it("visits admin customer page", () => {
+        cy.login('LC01', 'Yogesh@123');
+        cy.get('app-portal .cust').click();
+        cy.url().should('includes', 'admin/customers');
+        cy.get('.editcust').first().next().click({ multiple: true});
+    })
+
+    
+})
