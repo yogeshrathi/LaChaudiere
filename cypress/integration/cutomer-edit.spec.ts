46fcbdf9b1f56bde49e086e50cb606ecdfdb201a
Sunny Kumar <sunny@needleinnovision.com>
2022-04-20 15:28:22 +0530

    customer edit case scenario covered

--- cypress/integration/cutomer-edit.spec.ts
+++ cypress/integration/cutomer-edit.spec.ts
@@ -44,43 +44,48 @@ describe("Customer edit scenarios", () => {
         cy.login('LC01', 'Yogesh@123');
         cy.url().should('includes', 'admin/portal');
         cy.get("h2").should("contain.text", "Home");
+        cy.get('app-portal #customers').click();
+        cy.url().should('includes', 'admin/customers')
     })
 
-    it("open manage customer page ", () => {
-        cy.get('app-portal #customers').click();
-        cy.url().should('includes', 'admin/customers');
-    })
 
     it("search and open desired customer", () => {
-        cy.xpath('/html/body/app-root/app-admin/app-customers/section/div/div[1]/form/input').type('LC3879');
+        cy.xpath('/html/body/app-root/app-admin/app-customers/section/div/div[1]/form/input').clear().type('LC3879');
         cy.xpath('//*[@id="customers_0"]').click();
     });
 
     it("search and open desired customer", () => {
-        cy.get('#clientId').type('LC3879');
-        cy.get('#name').type('TANIA CHAMPAGNE');
-        cy.get('#email').type('epicerieshenley@hotmail.com');
-        cy.get('#password').type('12345678');
-        cy.get('#confirmPassword').type('12345678');
-        cy.get('#phone').type('(418)485-6813');
-        cy.get('#address').type('441, RUE PRINCIPALE');
-        cy.get('#postalCode').type('G0M 1V0');
-        cy.get('#city').type('ST-HONORE');
-        cy.get('#client').type('Client');
-        cy.get('#location').type('62');
-        cy.get('#monday').type('monday');
-        cy.get('#saveCustomer').click();
+        cy.get('#clientId').clear().type('LC3879');
+        cy.get('#name').clear().type('TANIA CHAMPAGNE');
+        cy.get('#email').clear().type('epicerieshenley@hotmail.com');
+        cy.get('#password').clear().type('12345678');
+        cy.get('#confirmPassword').clear().type('12345678');
+        cy.get('#phone').clear().type('(418)485-6813');
+        cy.get('#address').clear().type('441, RUE PRINCIPALE');
+        cy.get('#postalCode').clear().type('G0M 1V0');
+        cy.get('#city').clear().type('ST-HONORE');
+        cy.get('#companyName').clear().type('testing');
+        cy.get('#location').clear().type('62');
+        cy.get('input[type=submit]').click();
     });
 
-    // it("search and open editted product", () => {
-    //     cy.xpath('/html/body/app-root/app-admin/app-products/section/div/div[1]/form/input').clear().type("iPhone 13 Pro");
-    //     cy.xpath('/html/body/app-root/app-admin/app-products/section/div/div[2]/div[2]/div[4]/a').click();
-    // });
+    it("search and open editted customer", () => {
+        cy.xpath('/html/body/app-root/app-admin/app-customers/section/div/div[1]/form/input').clear().type("LC3879");
+        cy.xpath('//*[@id="customers_0"]').click();
+    });
 
-    // it("verify if product is editted", () => {
-    //     cy.get('input[name="product_name"]').should('have.value','iPhone 13 Pro');
-    //     cy.get('input[name="imageUrl"]').should('have.value','https://www.deccanherald.com/sites/dh/files/articleimages/2021/10/26/aiphone13pm-fcvs-sel-1-1037991-1635242483.jpg');
-    //     cy.get('input[name="description"]').should('have.value','Apple\'s latest model');
-    // });
+    it("verify if customer is editted", () => {
+        cy.get('#clientId').should('have.value','LC3879');
+        cy.get('#name').should('have.value','TANIA CHAMPAGNE');
+        cy.get('#email').should('have.value','epicerieshenley@hotmail.com');
+        cy.get('#password').should('have.value','12345678');
+        cy.get('#confirmPassword').should('have.value','12345678');
+        cy.get('#phone').should('have.value','(418)485-6813');
+        cy.get('#address').should('have.value','441, RUE PRINCIPALE');
+        cy.get('#postalCode').should('have.value','G0M 1V0');
+        cy.get('#city').should('have.value','ST-HONORE');
+        cy.get('#companyName').should('have.value','testing');
+        cy.get('#location').should('have.value','62');
+    });
 })
 
