ff4598e5f120e8bb9c70d6d7ac7af5d7192fd5c8
Nitin Daddikar <nitindaddikar@gmail.com>
2022-04-20 13:16:09 +0530

    edit product scenario

--- cypress/integration/admin-products.spec.ts
+++ cypress/integration/admin-products.spec.ts
@@ -16,16 +16,16 @@ describe("product add scenario", () => {
     });
 
     it("enter product details", () => {
-        cy.get('input[name="product_name"]').type('iPhone 13 Max Pro');
-        cy.get('input[name="imageUrl"]').type('https://www.deccanherald.com/sites/dh/files/articleimages/2021/10/26/aiphone13pm-fcvs-sel-1-1037991-1635242483.jpg');
-        cy.get('input[name="description"]').type('Apple\'s latest model');
+        cy.get('input[name="product_name"]').type('iPhone 13');
+        cy.get('input[name="imageUrl"]').type('https://www.imore.com/sites/imore.com/files/styles/xlarge_wm_brw/public/field/image/2021/09/iphone-13-and-mini-dark-and-light-hero.jpeg');
+        cy.get('input[name="description"]').type('Apple\'s new model');
         cy.get('input[name="isAvailable"]').check({ force: true })
         cy.get('input[type=submit]').click();
     });
 
     it("verify if product is added", () => {
-        cy.xpath('/html/body/app-root/app-admin/app-products/section/div/div[1]/form/input').type("iPhone 13 Max Pro");
-        cy.xpath('/html/body/app-root/app-admin/app-products/section/div/div[2]/div[2]/div[2]').should("contain.text", "iPhone 13 Max Pro");
+        cy.xpath('/html/body/app-root/app-admin/app-products/section/div/div[1]/form/input').type("iPhone 13");
+        cy.xpath('/html/body/app-root/app-admin/app-products/section/div/div[2]/div[2]/div[2]').should("contain.text", "iPhone 13");
     });
 })
 
@@ -40,4 +40,27 @@ describe("product edit scenarios", () => {
         cy.get('app-portal .prod').click();
         cy.url().should('includes', 'admin/products');
     })
+
+    it("search and open desired product", () => {
+        cy.xpath('/html/body/app-root/app-admin/app-products/section/div/div[1]/form/input').type("iPhone 13");
+        cy.xpath('/html/body/app-root/app-admin/app-products/section/div/div[2]/div[2]/div[4]/a').click();
+    });
+
+    it("search and open desired product", () => {
+        cy.get('input[name="product_name"]').type(' Pro');
+        cy.get('input[name="imageUrl"]').clear().type('https://www.deccanherald.com/sites/dh/files/articleimages/2021/10/26/aiphone13pm-fcvs-sel-1-1037991-1635242483.jpg');
+        cy.get('input[name="description"]').clear().type('Apple\'s latest model');
+        cy.get('input[type=submit]').click();
+    });
+
+    it("search and open editted product", () => {
+        cy.xpath('/html/body/app-root/app-admin/app-products/section/div/div[1]/form/input').clear().type("iPhone 13 Pro");
+        cy.xpath('/html/body/app-root/app-admin/app-products/section/div/div[2]/div[2]/div[4]/a').click();
+    });
+
+    it("verify if product is editted", () => {
+        cy.get('input[name="product_name"]').should('have.value','iPhone 13 Pro');
+        cy.get('input[name="imageUrl"]').should('have.value','https://www.deccanherald.com/sites/dh/files/articleimages/2021/10/26/aiphone13pm-fcvs-sel-1-1037991-1635242483.jpg');
+        cy.get('input[name="description"]').should('have.value','Apple\'s latest model');
+    });
 })
